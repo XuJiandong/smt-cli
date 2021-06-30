162d457c9f1ea7ca143eb027778767aa58c42633
xjd <xjd@cryptape.com>
2021-06-30 17:44:27 +0800

    draft

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # smt-cli
 Command line tools for SMT: https://github.com/jjyr/sparse-merkle-tree
+
+# inclusion
+```text
+smt-cli --include "0|1" 11 22
+```
+Generate a sparse merkel tree, with hash values of:
+```text
+[11, 0, 0, ...]
+```
+and 
+```text
+[22, 0, 0, ...]
+```
+and generate root and proof, which show the hash at index 0 and 1 are on tree.
+It's used for white list.
+
+The arguments in "--include" are indexes, delimited by |.
+# non-inclusion
+```text
+smt-cli --exclude "11|22" 111 222
+```
+Generate a sparse merkel tree, with hash values of:
+```text
+[111, 0, 0, ...]
+```
+and
+```text
+[222, 0, 0, ...]
+```
+and generate root and proof, which show the hash:
+```text
+[11, 0, 0, ...]
+```
+and 
+```text
+[22, 0, 0, ...]
+```
+are not on tree. It's used for black list.
+
+The arguments in "--exclude" are hashes, delimited by |.
+
+# Hash format on command line
+There are 2 places in command line to show hash:
+1. --exclude
+2. trailing arguments
+
+There are 2 formats to represent it:
+* hex format
+  which is started with "0x", the followed by hex string. For example, 
+   * 0x0
+   * 0xFFEF
+    Normally they are used as string in Rust or C.
+     
+* array format
+  plain number, delimited by ",". For example, 
+   * 100,200,300
+   * 0
+    Normally they are used as literal in Rust or C.
+     
+padding with zero if the length is smaller than 32.
---
+++ src/main.rs
@@ -0,0 +1,293 @@
+mod blake2b;
+
+use blake2b::Blake2bHasher;
+use clap::{App, AppSettings, Arg};
+use lazy_static::lazy_static;
+use sparse_merkle_tree::default_store::DefaultStore;
+use sparse_merkle_tree::{SparseMerkleTree, H256};
+
+lazy_static! {
+    pub static ref NON_INCLUSION: H256 = [0; 32].into();
+    pub static ref INCLUSION: H256 = [
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0
+    ]
+    .into();
+}
+const DELIMITER: &str = "|";
+
+fn hex_format(hash: &[u8]) -> String {
+    let strs: Vec<String> = hash.into_iter().map(|h| format!("{:x}", h)).collect();
+    String::from("0x") + &strs.join("")
+}
+
+fn array_format(hash: &[u8]) -> String {
+    let strs: Vec<String> = hash.into_iter().map(|h| format!("{}", h)).collect();
+    strs.join(",")
+}
+
+// sample
+// 1|2|3
+fn parse_index(arg: &str) -> Vec<usize> {
+    let arg = arg.trim();
+    fn parse(s: &str) -> usize {
+        let res = s.trim().parse::<usize>();
+        if res.is_err() {
+            println!(
+                "error: \"{}\" is not a number: please delimited number by |.",
+                s
+            );
+            panic!("stop");
+        } else {
+            res.unwrap()
+        }
+    }
+    arg.split(DELIMITER).into_iter().map(|i| parse(i)).collect()
+}
+
+// append 0 at the end
+fn parse_hash(arg: &str) -> H256 {
+    let arg = arg.trim().to_lowercase();
+    let mut result = [0; 32];
+    fn parse(c: &[u8]) -> u8 {
+        let str = String::from_utf8_lossy(c);
+        let res = u8::from_str_radix(str.as_ref(), 16);
+        if res.is_err() {
+            println!("error, \"{}\" is not a valid hex", str);
+            panic!("stop");
+        } else {
+            res.unwrap()
+        }
+    }
+
+    if arg.len() > 2 && "0x" == &arg[0..2] {
+        let arg2 = arg.split_at(2).1;
+        let r: Vec<u8> = arg2.as_bytes().chunks(2).map(|c| parse(c)).collect();
+        &result[..r.len()].copy_from_slice(r.as_slice());
+    } else {
+        let r: Vec<u8> = arg
+            .split(",")
+            .into_iter()
+            .map(|i| i.parse::<u8>().unwrap())
+            .collect();
+        &result[..r.len()].copy_from_slice(r.as_slice());
+    }
+    result.into()
+}
+
+// sample
+// 0xFF|0xEEFF00|...
+// 12,23,45|0|255,255,255,255|...
+// 0xFF|12|0
+fn parse_hashes(arg: &str) -> Vec<H256> {
+    let arg = arg.trim();
+    arg.split(DELIMITER)
+        .into_iter()
+        .map(|i| parse_hash(i))
+        .collect()
+}
+
+#[test]
+fn test_parse_hashes() {
+    let arg = "0xFF|0x0|0x1234567890";
+    let hashes = parse_hashes(arg);
+    assert_eq!(
+        hashes,
+        vec![
+            [
+                0xff, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0
+            ]
+            .into(),
+            [0; 32].into(),
+            [
+                0x12, 0x34, 0x56, 0x78, 0x90, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0
+            ]
+            .into()
+        ]
+    );
+    let arg2 = "0xFF|0|12,34,56,78,90";
+    let hashes2 = parse_hashes(arg2);
+    assert_eq!(
+        hashes2,
+        vec![
+            [
+                0xff, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0
+            ]
+            .into(),
+            [0; 32].into(),
+            [
+                12, 34, 56, 78, 90, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0
+            ]
+            .into()
+        ]
+    );
+}
+
+#[test]
+fn test_parse_hashes2() {
+    let arg = "255 | 0 |12,34,56,78,90";
+    let hashes = parse_hashes(arg);
+    assert_eq!(
+        hashes,
+        vec![
+            [
+                255, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0
+            ]
+            .into(),
+            [0; 32].into(),
+            [
+                12, 34, 56, 78, 90, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0
+            ]
+            .into()
+        ]
+    );
+}
+
+#[test]
+fn test_parse_index() {
+    let arg = "0|1|2";
+    let indexes = parse_index(arg);
+    assert_eq!(indexes, vec![0, 1, 2]);
+}
+
+fn main() {
+    let matches = App::new("smt-cli")
+        .version("1.0")
+        .about("Sparse Merkel Tree command line tool")
+        .setting(AppSettings::TrailingVarArg)
+        .arg(
+            Arg::with_name("include")
+                .required(false)
+                .short("i")
+                .long("include")
+                .value_name("INDEXES")
+                .takes_value(true)
+                .help("Indexes of hashes to include, e.g. 1 | 2 | 3, delimited by |"),
+        )
+        .arg(
+            Arg::with_name("exclude")
+                .required(false)
+                .short("e")
+                .long("exclude")
+                .value_name("HASHES")
+                .takes_value(true)
+                .help("Hashes to exclude, delimited by |"),
+        )
+        .arg(
+            Arg::with_name("hex")
+                .required(false)
+                .short("x")
+                .long("hex")
+                .help("print data in hex string format, starting with 0x. The default is array string format."),
+        )        // trailing args
+        .arg(Arg::with_name("hashes")
+            .value_name("HASHES")
+            .multiple(true)
+        )
+        .get_matches();
+
+    let include = matches.value_of("include");
+    let exclude = matches.value_of("exclude");
+    let is_hex_format = matches.is_present("hex");
+    if include.is_some() && exclude.is_some() {
+        println!("include and exclude can't be both used.");
+        println!("{}", matches.usage());
+        return;
+    } else if include.is_none() && exclude.is_none() {
+        println!("must specify include or exclude.");
+        println!("{}", matches.usage());
+        return;
+    }
+    let hashes_str: Vec<&str> = matches.values_of("hashes").unwrap().collect();
+    let hashes: Vec<H256> = hashes_str.into_iter().map(|h| parse_hash(h)).collect();
+
+    let (root, proof) = if include.is_some() {
+        let index = parse_index(include.unwrap());
+        make_inclusion_proof(hashes, index)
+    } else {
+        assert!(exclude.is_some());
+        let non_inclusion_hashes = parse_hashes(exclude.unwrap());
+        make_none_inclusion_proof(hashes, non_inclusion_hashes)
+    };
+    if is_hex_format {
+        println!("root: {}", hex_format(root.as_slice()));
+        println!("proof: {}", hex_format(proof.as_slice()));
+    } else {
+        println!("root: {}", array_format(root.as_slice()));
+        println!("proof: {}", array_format(proof.as_slice()));
+    }
+}
+
+type SMT = SparseMerkleTree<Blake2bHasher, H256, DefaultStore<H256>>;
+
+fn new_smt(pairs: Vec<(H256, H256)>) -> SMT {
+    let mut smt = SMT::default();
+    for (key, value) in pairs {
+        smt.update(key, value).unwrap();
+    }
+    smt
+}
+
+fn make_inclusion_proof(hashes: Vec<H256>, indexes: Vec<usize>) -> (H256, Vec<u8>) {
+    let inclusion_hashes: Vec<(H256, H256)> = hashes
+        .clone()
+        .into_iter()
+        .map(|h| (h, INCLUSION.clone()))
+        .collect();
+    let smt = new_smt(inclusion_hashes.clone());
+    let root = smt.root();
+
+    let inclusion_pairs: Vec<(H256, H256)> = indexes
+        .into_iter()
+        .map(|i| (hashes[i], INCLUSION.clone()))
+        .collect();
+    let inclusion = inclusion_pairs
+        .clone()
+        .into_iter()
+        .map(|(k, _)| k)
+        .collect();
+
+    let proof = smt.merkle_proof(inclusion).expect("gen proof");
+    let compiled_proof = proof
+        .clone()
+        .compile(inclusion_pairs.clone())
+        .expect("compile proof");
+    // verify it locally
+    assert!(compiled_proof
+        .verify::<Blake2bHasher>(smt.root(), inclusion_pairs)
+        .expect("verify compiled proof"));
+
+    (root.clone(), compiled_proof.0)
+}
+
+fn make_none_inclusion_proof(hashes: Vec<H256>, non_inclusion: Vec<H256>) -> (H256, Vec<u8>) {
+    let inclusion_hashes: Vec<(H256, H256)> = hashes
+        .clone()
+        .into_iter()
+        .map(|h| (h, INCLUSION.clone()))
+        .collect();
+    let smt = new_smt(inclusion_hashes.clone());
+    let root = smt.root();
+
+    let proof = smt.merkle_proof(non_inclusion.clone()).expect("gen proof");
+    let non_inclusion_pairs: Vec<(H256, H256)> = non_inclusion
+        .into_iter()
+        .map(|i| (i, NON_INCLUSION.clone()))
+        .collect();
+    let compiled_proof = proof
+        .clone()
+        .compile(non_inclusion_pairs.clone())
+        .expect("compile proof");
+    // verify it locally
+    assert!(compiled_proof
+        .verify::<Blake2bHasher>(smt.root(), non_inclusion_pairs)
+        .expect("verify compiled proof"));
+
+    (root.clone(), compiled_proof.0)
+}
